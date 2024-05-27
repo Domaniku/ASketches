47fc1442355e3bf81a71cd467881ce4edb8e4db0
Domaniku <Domaniklavonj@gmail.com>
2024-05-27 13:08:57 -0400

    Update RBG.ino

--- Blink_LED/RBG.ino
+++ Blink_LED/RBG.ino
@@ -1,1 +1,29 @@
 
+int red = 1;
+int blue = 2;
+int green = 3;
+
+void setup() {
+  
+    pinMode(red, OUTPUT);
+    pinMode(blue, OUTPUT);
+    pinMode(green, OUTPUT);
+  
+}
+
+
+void loop() {
+  
+  digitalWrite(red, HIGH);
+  digitalWrite(blue, LOW);
+  digitalWrite(green, LOW);
+  delay(1000);
+  digitalWrite(blue, High);
+  digitalWrite(red, LOW);
+  digitalWrite(green, LOW);
+  delay(1000);
+  digitalWrite(blue, LOW);
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+
+}
